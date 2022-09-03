da489e9aedc0ddf2af3f5a974c8a2786ac249c86
Arnaud Lafont <arnaud-lafont@hotmail.fr>
2022-09-04 01:45:53 +0200

    ajout de la page indice

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+app.config['img_folder'] = os.path.join('static', 'images')
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 @app.route('/matrix')
 def table():
     return render_template('matrix.html')
+
+
+@app.route('/clue')
+def clue():
+    imgs = [
+        os.path.join(app.config['img_folder'], 'map.jpg'),
+        os.path.join(app.config['img_folder'], 'photo01.jpg')
+    ]
+    return render_template('clue.html', imgs=imgs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
